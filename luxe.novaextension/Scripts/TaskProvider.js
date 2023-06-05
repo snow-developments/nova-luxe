8376ddb9d09cbcb1ace15f4935e5e8f25bfff7e6
Chance Snow <git@chancesnow.me>
2023-06-05 18:38:25 -0500

    Add a luxe Run task configuration

---
+++ luxe.novaextension/Scripts/TaskProvider.js
@@ -0,0 +1,44 @@
+const HOME = nova.environment["HOME"] || "~";
+
+// Application types
+// See https://en.wikipedia.org/wiki/Uniform_Type_Identifier
+const MAC_APP = "app";
+const UNIX_APP = "public.unix-executable";
+
+const LUXE_SDK = `${HOME}/.luxe`;
+const LUXE_EXE = `${LUXE_SDK}/bin/luxe`;
+
+class TaskProvider {
+  constructor() {}
+
+  /**
+   * @param context TaskActionResolveContext
+   */
+  async resolveTaskAction(context) {
+    // TODO: Support `build` and `deploy` tasks
+    if (context.action !== Task.Run) return null;
+    // TODO: Are there things to cleanup on behalf of luxe?
+    // else if (context.action.type === Task.Clean)
+    // See https://docs.nova.app/api-reference/configuration/#getkey-coerce
+    const config = context.config;
+
+    let launchPath = config.get("launchPath", "string") || "project.luxe";
+    if (!launchPath) throw "Invalid luxe task configuration: You must provide a Program to launch or attach to.";
+    /** @type string[] */
+    const launchArgs = config.get("launchArgs", "array") || [];
+    const cwd = nova.path.normalize(config.get("cwd", "string") || nova.workspace.path);
+
+    const action = new TaskProcessAction(LUXE_EXE, {
+      args: ["run", launchPath].concat(launchArgs),
+      stdio: ["ignore", "pipe", "pipe"],
+      cwd,
+      // TODO: Forward errors to a problem matcher
+      matchers: []
+    });
+
+    // Launch the debugee
+    console.log(`launching luxe app: "${launchPath}"`);
+    return action;
+  }
+}
+exports.TaskProvider = TaskProvider;
--- luxe.novaextension/Scripts/main.js
+++ luxe.novaextension/Scripts/main.js
@@ -1,38 +1,14 @@
+const { TaskProvider } = require("./TaskProvider");
 
 exports.activate = function() {
-  // Do work when the extension is activated
+  console.log("activated");
 }
 
 exports.deactivate = function() {
   // Clean up state before the extension is deactivated
 }
 
-class TaskProvider {
-  constructor() {}
-
-  provideTasks() {
-    let task = new Task("Say Example");
-
-    task.setAction(Task.Build, new TaskProcessAction('/usr/bin/say', {
-      args: ["I'm Building!"],
-      env: {}
-    }));
-
-    task.setAction(Task.Run, new TaskProcessAction('/usr/bin/say', {
-      args: ["I'm Running!"],
-      env: {}
-    }));
-
-    task.setAction(Task.Clean, new TaskProcessAction('/usr/bin/say', {
-      args: ["I'm Cleaning!"],
-      env: {}
-    }));
-
-    return [task];
-  }
-}
-
 nova.assistants.registerTaskAssistant(new TaskProvider(), {
-  identifier: "example-tasks",
-  name: "Examples"
+  identifier: "luxe",
+  name: "luxe"
 });
