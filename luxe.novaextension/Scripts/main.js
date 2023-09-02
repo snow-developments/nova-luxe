73278196836e4c31f8b0d9dfb427e6d1bfe03236
Chance Snow <git@chancesnow.me>
2023-09-02 17:09:23 -0500

    Update TODOs

--- luxe.novaextension/Scripts/main.js
+++ luxe.novaextension/Scripts/main.js
@@ -1,5 +1,12 @@
 const { TaskProvider } = require("./TaskProvider");
 
+// TODO: Add menus to create new assets: e.g. Materials, Shaders, etc.
+// These actions should create new `*.lx` files, e.g. `player.material.lx`
+// Also add snippets for these asset resources for `*.lx` files
+
+// TODO: Add autocompletes for luxe built-in assets, e.g. `luxe: material_basis/solid`
+// These are basically references to `*.lx` resources in https://github.com/luxeengine/luxe-module/tree/main/assets
+
 exports.activate = function() {
   console.log("activated");
 }
